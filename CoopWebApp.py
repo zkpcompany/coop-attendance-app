90999d59e77d8aab560266d7fd6cb0d04ec1f1a5
zkpcompany <zkpcompany@gmail.com>
2026-07-18 15:14:22 -0400

    Update CoopWebApp.py

--- CoopWebApp.py
+++ CoopWebApp.py
@@ -47,7 +47,7 @@ st.markdown(
         position: relative;
         top: -50px;
     ">
-        <img src="static/EnterzLogo.png"
+        <img src="https://raw.githubusercontent.com/zkpcompany/enterz/main/EnterzLogo.png"
              style="height: 50px; margin-right: 12px;">
         <span style="
             font-size: 28px;
